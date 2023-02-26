3477e96f206955c5200647d9a26caea093444d4c
Akingbade Motolani <akingbademotolani@yahoo.co.uk>
2023-02-27 00:51:44 +0100

    take_1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Task-Management-Project
+
+This is a project for managing tasks. It is a simple project that uses the following technologies:
+- GrapghQL
+- Apollo
+- Typescript
+
+## Installation
+- Reference the apollo documentation to kick newly
+- Clone the repo
+- built on yarn 
+- cd to the server repo
+- run `yarn install`
+- run `yarn start`/ `yarn test` to start server
+- run `yarn dev` to compile 
--- server/dist/index.js
+++ server/dist/index.js
@@ -1,49 +1,134 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-const books = [
-    {
-        title: "The Awakening",
-        author: "Kate Chopin",
-    },
-    {
-        title: "City of Glass",
-        author: "Paul Auster",
-    },
-];
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+dotenv.config();
+const { DB_NAME, DB_URI } = process.env;
+const typeDefs = `
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+type Query {
+ myTaskLists : [TaskList!]!
+}
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
+type Mutation {
+ signup(input: SignUpInput): AuthUser!
+ signin(input: SignInInput): AuthUser!
+}
+
+input SignUpInput {
+ email: String!
+ password:String!
+ name: String!
+ avatar:String
+}
+
+input SignInInput {
+ email: String!
+ password:String!
+}
+
+type AuthUser {
+ user: User!
+ token: String!
+}
+
+type User {
+  id: ID!
+  name: String!
+  email: String!
+  avatar: String
+}
+
+type TaskList {
+  id: ID!
+  createdAt: String!
+  title: String!
+  progress: Float!
+
+  users: [User!]!
+  todos: [ToDo!]!
+}
+
+type ToDo {
+ id: ID!
+ content: String!
+ isCompleted: Boolean!
+
+ taskList: TaskList!
+}
 `;
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
+// Resolvers define how to fetch the types defined in your schema using queries
 const resolvers = {
     Query: {
-        books: () => books,
+        myTaskLists: () => [],
+    },
+    Mutation: {
+        signup: async (_, { input }, { db }) => {
+            const hashedPassword = bcrypt.hashSync(input.password);
+            const user = {
+                ...input,
+                password: hashedPassword,
+            };
+            // save to database and insert user to database
+            const result = await db.collection("users").insertOne(user);
+            // console.log(result);
+            return {
+                user: {
+                    ...user,
+                },
+                token: "token",
+            };
+        },
+        signin: async (_, { input }, { db }) => {
+            const user = await db.collection('users').findOne({ email: input.email });
+            if (!user) {
+                throw new Error('User not found');
+            }
+            // check if password is correct
+            const isPasswordCorrect = bcrypt.compareSync(input.password, user.password);
+            if (!isPasswordCorrect) {
+                throw new Error('User not found');
+            }
+            return {
+                user,
+                token: "token"
+            };
+        },
+    },
+    User: {
+        // id: (root) => {
+        //   return root._id || root.id
+        // },
+        id: ({ _id, id }) => id || _id
     },
 };
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-});
-console.log(`🚀  Server ready at: ${url}`);
+const start = async () => {
+    const client = new MongoClient(DB_URI, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+        },
+    });
+    await client.connect();
+    const db = client.db(DB_NAME);
+    const context = db;
+    console.log(`Connected to database`);
+    // The ApolloServer constructor requires two parameters: your schema
+    // definition and your set of resolvers.
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    // Passing an ApolloServer instance to the `startStandaloneServer` function:
+    //  1. creates an Express app
+    //  2. installs your ApolloServer instance as middleware
+    //  3. prepares your app to handle incoming requests
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+        context: async () => ({
+            db,
+        }),
+    });
+    console.log(`🚀  Server ready at: ${url}`);
+};
+start();
